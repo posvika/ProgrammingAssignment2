fe014f585678fdafbd13508b3955a670e889a77f
posvika <pow_wow@ro.ru>
2015-02-23 03:13:10 +0300

    #assigment done, ver.1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing an inverse matrix with caxhe for "R programming" assigment 2 on Coursera
 
-## Write a short comment describing this function
+## Uses solve() function with single argument to compute inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	cache <- NULL
+	set <- function(y) {
+                x <<- y
+                cache <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverseMatrix) cache <<- solve
+        getInv <- function() cache
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        cache <- x$getInv()
+        if(!is.null(cache)) {
+                message("getting cached data")
+                return(cache)
+        }
+        data <- x$get()
+        cache <- solve(data)
+        x$setInv(cache)
+        cache
+
 }
